f7e6a17898e60f02734a3f079da8e66b2701ead5
dnovaes <novaesdigo@gmail.com>
2020-01-13 21:40:19 -0300

    Create DateExtension for conversions

---
+++ app/src/main/java/com/kaefer/convertdateslab/DateExtension.kt
@@ -0,0 +1,17 @@
+package com.kaefer.convertdateslab
+
+import org.threeten.bp.LocalDate
+import java.util.Calendar
+import java.util.Date
+import java.util.GregorianCalendar
+
+fun Date.toLocalDate(): LocalDate {
+    val calendar = GregorianCalendar()
+    calendar.time = this
+
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH)+1
+    val day = calendar.get(Calendar.DAY_OF_MONTH)
+    return LocalDate.of(year, month, day)
+}
+
