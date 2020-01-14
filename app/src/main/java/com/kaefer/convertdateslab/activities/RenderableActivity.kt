1806cbd944ae141115e85924d31c10b61f852b9d
dnovaes <novaesdigo@gmail.com>
2020-01-13 21:45:54 -0300

    Create base activity containing an renderable view

---
+++ app/src/main/java/com/kaefer/convertdateslab/activities/RenderableActivity.kt
@@ -0,0 +1,22 @@
+package com.kaefer.convertdateslab.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jakewharton.threetenabp.AndroidThreeTen
+import trikita.anvil.RenderableView
+
+abstract class RenderableActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AndroidThreeTen.init(this)
+        setContentView(object: RenderableView(this) {
+            override fun view() {
+                content()
+            }
+        })
+    }
+
+    abstract fun content()
+
+}
